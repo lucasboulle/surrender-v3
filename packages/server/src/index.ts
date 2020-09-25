35fa234e929031d73a2dc353bb55634bdc6b2541
Lucas Boulle <lucas.figueiredo@ammovarejo.com.br>
2020-09-25 00:32:49 -0300

    Set server port to listen

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -8,6 +8,6 @@ const app = express()
 app.use(cors())
 app.use(routes)
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000')
+app.listen(8080, () => {
+  console.log('Server running on port 8080')
 })
