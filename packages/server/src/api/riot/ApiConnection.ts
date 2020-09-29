2f4168fe663c747fbfc2098d0351761d718610bc
Lucas Figueiredo <lucas.figueiredo@ammovarejo.com.br>
2020-09-29 12:53:40 -0300

    Update development api key

--- packages/server/src/api/riot/ApiConnection.ts
+++ packages/server/src/api/riot/ApiConnection.ts
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 const apiBaseUrl = 'https://br1.api.riotgames.com/lol'
 
 // TODO: Find another way to use this key
-const apiKey = 'RGAPI-bc505357-9ccb-4f4a-89ba-e8e2d81ab4b5'
+const apiKey = 'RGAPI-d71e9d04-f819-43c5-aa28-32b2411dce8c'
 
 export const getRequest = async (path: string, queryParams?: string[][]) => {
   const response = await fetch(apiBaseUrl + path + `?api_key=${apiKey}`, {
