8143d901376e9c5cbd160c2e281449db06b6090f
Lucas Boulle <mr.boullefigueiredo@gmail.com>
2020-11-02 11:16:01 -0300

    Add barchart and adjust layout issues to profile page

--- packages/web/src/pages/Profile/index.tsx
+++ packages/web/src/pages/Profile/index.tsx
@@ -8,7 +8,8 @@ import {
   ContentContainer,
   PlayedChampions,
   ChampionIcon,
-  MatchList
+  MatchList,
+  ImageContainer
 } from './styles'
 import {
   createStyles,
@@ -18,13 +19,16 @@ import {
   IconButton,
   LinearProgress,
   makeStyles,
-  Theme
+  Theme,
 } from '@material-ui/core'
 import challengerIcon from '../../images/challenger-icon.jpeg'
 import rankedEmblem from '../../images/ranked-emblems/Emblem_Master.png'
 import rivenExample from '../../images/riven-example.jpeg'
 import rivenExampleIcon from '../../images/riven-example-icon.jpeg'
-import { InfoRounded } from '@material-ui/icons'
+import blueWaves from '../../images/blue-waves.png'
+import { InfoRounded, PieChart } from '@material-ui/icons'
+import { CartesianGrid, BarChart, XAxis, YAxis, Tooltip, Bar } from 'recharts'
+
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -44,7 +48,11 @@ const useStyles = makeStyles((theme: Theme) =>
     progressBar: {
       maxHeight: 0,
       minWidth: '99%'
+    },
+    radiusChart: {
+      width: 50
     }
+
   })
 )
 
@@ -59,6 +67,30 @@ const Profile: React.FC = () => {
     { dale: 'dale' }
   ])
 
+  const data = [
+    {
+      name: 'Page A', pv: 2400, amt: 2400,
+    },
+    {
+      name: 'Page B', pv: 1398, amt: 2210,
+    },
+    {
+      name: 'Page C', pv: 9800, amt: 2290,
+    },
+    {
+      name: 'Page D', pv: 3908, amt: 2000,
+    },
+    {
+      name: 'Page E', pv: 4800, amt: 2181,
+    },
+    {
+      name: 'Page F', pv: 3800, amt: 2500,
+    },
+    {
+      name: 'Page G', pv: 4300, amt: 2100,
+    },
+  ]
+
   const fetchMatchList = React.useCallback(
     async (term?: string) => {
       const matchList = null
@@ -69,6 +101,10 @@ const Profile: React.FC = () => {
 
   return (
     <Container>
+      <ImageContainer>
+        <img src={blueWaves} style={{rotate: '180deg', background: '#4c566a'}} />
+      </ImageContainer>
+
       <ProfileData>
         <ContentContainer>
           <RankEmblemImage src={rankedEmblem} />
@@ -82,27 +118,40 @@ const Profile: React.FC = () => {
           <ProfileInfo>378</ProfileInfo>
         </ContentContainer>
 
-        <ContentContainer></ContentContainer>
+        <ContentContainer>
+          <BarChart width={400} height={200} data={data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name"  />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="pv" fill="#8884d8" />
+          </BarChart>
+        </ContentContainer>
       </ProfileData>
+
       <PlayedChampions>
         <ContentContainer>
           <ChampionIcon src={rivenExampleIcon} />
           <ProfileInfo>Master</ProfileInfo>
         </ContentContainer>
+
         <ContentContainer>
           <ChampionIcon src={rivenExampleIcon} />
           <ProfileInfo>Master</ProfileInfo>
         </ContentContainer>
+
         <ContentContainer>
           <ChampionIcon src={rivenExampleIcon} />
           <ProfileInfo>Master</ProfileInfo>
         </ContentContainer>
       </PlayedChampions>
-      <MatchList>
+
+      <MatchList> 
         <GridList cellHeight={180} className={classes.gridList} cols={3}>
           {matchList ? (
             matchList.map((match: any, index: number) => (
-              <GridListTile key={1}>
+              <GridListTile key={index}>
                 <img src={rivenExample} />
                 <GridListTileBar
                   title={'3/7/9 - 50:12'}
--- packages/web/src/pages/Profile/styles.ts
+++ packages/web/src/pages/Profile/styles.ts
@@ -8,11 +8,12 @@ height: 100%;
 display: flex;
 flex-direction: column;
 align-items: sretch;
+justidy-content: flex-start;
 padding: 0 0 0 0;
 `;
 
 export const ProfileData = styled.div`
-height: 30%;
+height: 50%;
 background: #4c566a;
 display: flex;
 flex-direction: row;
@@ -49,7 +50,7 @@ justify-content: center;
 `
 
 export const PlayedChampions = styled.div`
-height: 30%;
+height: 40%;
 background: #434c5e;
 display: flex;
 flex-direction: row;
@@ -60,16 +61,25 @@ border-bottom-style: solid;
 `
 
 export const MatchList = styled.div`
-height: 40%;
+height: 100px;
 background: #434c5e;
-display: flex;
-flex-direction: row;
-justify-content: space-between;
 `
 
 export const ChampionIcon = styled.img`
-width: 150px;
-height: 150px;
+width: 100px;
+height: 100px;
 border-radius: 150px;
 align-self: center;
 `
+
+export const ImageContainer = styled.div`
+  color: #2e3440;
+  background: #d8dee9;
+  width: 100%;
+  height: 17%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: sretch;
+  padding: 0 0 0 0;
+`;
