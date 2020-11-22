8ba4fd912ebaf70903c1c485de5a88c7462f79a4
Lucas Boulle <mr.boullefigueiredo@gmail.com>
2020-11-21 21:58:18 -0300

    Change cors access control header origin

--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -16,6 +16,9 @@ const unprotectedRoutes = [
 ]
 
 routes.use((request, response, next) => {
+  response.header("Access-Control-Allow-Origin", "*")
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  
   if(unprotectedRoutes.find(r => r === request.path)) {
     next()
   } else {
